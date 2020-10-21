aebe5214255b2e706c83c37db73fb6cdcdbc5e0f
Pailasowmya <69006691+Pailasowmya@users.noreply.github.com>
2020-10-21 20:18:59 +0530

    Add files via upload

---
+++ Que3/SavingAccount.java
@@ -0,0 +1,27 @@
+package assign2;
+
+public class SavingAccount extends Account {
+	private final static double INTEREST = 5;
+	private final static double MAX_WITHDRAW_LIMIT = 5000.0;
+
+	public SavingAccount(String memberName, double accountBalance) {
+		super(memberName, accountBalance);
+	}
+
+	public double getBalance() {
+		double interest;
+		if (getAccountBalance() >= 1000) {
+			interest = getAccountBalance() * INTEREST;
+		} else
+			interest = 0.0;
+		return interest;
+	}
+
+	public void withDraw(double amount) {
+		if ((getAccountBalance() - amount) >= 1000) {
+			setAccountBalance(getAccountBalance() - amount);
+		} else {
+			System.out.println("Insufficient funds");
+		}
+	}
+}
